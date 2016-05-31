73af06175166c5e5ec96b3d62cc6a610652ec227
EduardoPac <eduardopacheco0306@gmail.com>
2016-05-31 16:58:02 -0300

    ee
    ee

---
+++ fatoracaounica/Fatoracao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fatoracaounica
+{
+    class Fatoracao
+    {
+        public static List<int> fatorar(int n)
+        {
+            int f = 2;
+
+            List<int> fatores = new List<int>();
+
+            while(n > 1 && n >= (int)Math.Sqrt(n))
+            {
+                while((n%f)== 0)
+                {
+                    fatores.Add(f);
+                    n = n / f;
+                }
+                f++;
+            }
+
+            return fatores;
+        }
+    }
+}
---
+++ fatoracaounica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fatoracaounica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o numero que deseja fatorar");
+            int n = Int32.Parse(Console.ReadLine());
+
+            List<int> fatores = Fatoracao.fatorar(n);
+
+            Console.Write("{0} = ", n);
+
+            Console.Write("{0}", fatores[0]);
+
+            int size = fatores.Count;
+
+            for(int i=1;i<size;i++)
+            {
+                Console.Write(".{0}", fatores[i]);
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Pressione um botao para continuar....");
+
+            Console.ReadKey();
+        }
+    }
+}
